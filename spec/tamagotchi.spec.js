0662c2702d8b4204b62b4c53b4cf003e703d2b6e
Student <example@example.com>
2019-03-18 15:53:14 -0700

    fix beforeEach bug by including level resets

--- spec/tamagotchi.spec.js
+++ spec/tamagotchi.spec.js
@@ -6,11 +6,15 @@ describe('Tama', function() {
   beforeEach(function() {
     jasmine.clock().install();
     winston.setValueTimer("sportLevel", 2000);
-    // winston.setValueTimer("foodLevel", 1000);
+    winston.setValueTimer("foodLevel", 1000);
     console.log((winston.sportLevel + 'sport'), (winston.foodLevel + 'food') );
+    winston.setValue("sportLevel");
+    winston.setValue("foodLevel");
   });
+
   afterEach(function() {
     jasmine.clock().uninstall();
+    console.log("afterEach");
   });
 
   it('should have a name and all levels of 10 when it is created', function() {
@@ -19,18 +23,18 @@ describe('Tama', function() {
     expect(winston.sportLevel).toEqual(10);
 
   });
-
   it('should have a sport level of 5 after 10 seconds', function() {
     // jasmine.clock().tick(3001);
     // expect(winston.foodLevel).toEqual(7);
     jasmine.clock().tick(10001);
     expect(winston.sportLevel).toEqual(5);
   });
+  it('should have a food level of 6 after 4001 milliseconds', function() {
+    jasmine.clock().tick(4001);
+    expect(winston.foodLevel).toEqual(6);
+  });
 
-  // it('should have a food level of 7 after 3001 milliseconds', function() {
-  //   jasmine.clock().tick(3001);
-  //   expect(winston.foodLevel).toEqual(7);
-  // });
+
 
 
   it('should get very hungry if the food level drops below zero', function() {
@@ -41,10 +45,11 @@ describe('Tama', function() {
   it('should get very hungry if 10 seconds pass without feeding', function() {
     jasmine.clock().tick(10001);
     expect(winston.didYouGetStarved()).toEqual(true);
+    console.log(winston);
   });
 
   it('should have a food level of ten if it is fed', function() {
-    jasmine.clock().tick(9001);
+    jasmine.clock().tick(8001);
     winston.setValue("foodLevel");
     expect(winston.foodLevel).toEqual(10);
   });
@@ -56,7 +61,8 @@ describe('Tama', function() {
   });
 
   it('should get very bored if 20 seconds were to pass without sport', function() {
-    jasmine.clock().tick(20000);
+
+    jasmine.clock().tick(20001);
     expect(winston.didYouGetBored()).toEqual(true);
   });
 
