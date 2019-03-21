e3f5581a6fba2084006befd04d7537a3fbbe1e3d
Drew Yoxsimer and Slater Smith <slater@>
2019-03-21 13:54:58 -0700

    rewrite weather api code to read dino ipsum api calls

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    let city = $('#location').val();
-    $('#location').val("");
-    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`).then(function(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    let input = $('#dino1').val();
+    let input2 = $('#dino2').val();
+    $('#dino1').val("");
+    $('#dino2').val("");
+    $('#showErrors').val("");
+    $.get("http://dinoipsum.herokuapp.com/api/?format=json").then(function(response) {
+      let inputObj = response;
+      let inputNxt = response[input];
+      let inputDino = inputNxt[input2];
+      $('.showHumidity').text(`${inputNxt[input2]}`);
+      // $('.showTemp').text(`The temperature in Kelvins is ${response} degrees.`);
+      console.log(response."0"[5]);
     }).fail(function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
